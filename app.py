f804cc8e93ad4fa48cb97a22b1a624f36edae335
Bernard van der Wal <bernardvanderwal@gmail.com>
2023-01-18 11:05:47 -0300

    Recommiting env + changes to app.py

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
 
 # Instancing Flask + API
 app = Flask(__name__)
 api = Api(app)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+
+    def __repr__(self):
+        return self.name
 
 fakeDatabase = {
     1:{'name':'Clean car'},
